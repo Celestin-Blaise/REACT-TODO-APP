20ba7ce46d13832f8b8ff368a16b56439a9b1fbe
CELESTIN BLAISE <celestinblaise8@gmail.com>
2020-04-11 10:02:11 +0300

    run some cloud shell updates

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Todos from './Todos'
 import AddTodo from './AddTodo'
-class App extends Component{
-  state ={
-       todos: [
-         {id: 1,content:'Buy Milk In The Morning'},
-         {id: 2,content:'Make Some Tea'}
-       ]
-  }
-  
-  addTodo = (todo) =>{
-      todo.id=Math.random();
-      let todos=[...this.state.todos,todo]
-      this.setState({
-        todos
-      })
-  }
+class App extends Component {
+    state = {
+        todos: [
+            { id: 1, content: 'Buy Milk In The Morning' },
+            { id: 2, content: 'Make Some Tea' }
+        ]
+    }
 
-  deleteTodo = (id) =>{
-     const todos = this.state.todos.filter(todo => {
-       return todo.id !== id
-     });
-     this.setState({
-       todos
-     })
-  }
+    addTodo = (todo) => {
+        todo.id = Math.random();
+        let todos = [...this.state.todos, todo]
+        this.setState({
+            todos
+        })
+    }
 
-  render() {
-   return(
-     <div className="todo-app container ">
-     <h1 className="center blue-text">TODO'S</h1>
-     <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-     <AddTodo addTodo={this.addTodo}/>
-     </div>
-   )
-  }
+    deleteTodo = (id) => {
+        const todos = this.state.todos.filter(todo => {
+            return todo.id !== id
+        });
+        this.setState({
+            todos
+        })
+    }
+
+    render() {
+        return (
+            <div className="todo-app container ">
+                <h1 className="center blue-text">TODO APP</h1>
+                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+                <AddTodo addTodo={this.addTodo} />
+            </div>
+        )
+    }
 }
 
 export default App;
